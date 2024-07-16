681bbf288ec1e31822c93bada8d4392e4295d212
ashiqur-shohan <ashiqur.shohan@gmail.com>
2024-07-16 19:29:38 +0600

    make mobile responsive

--- src/Components/TechStack/TechStack.jsx
+++ src/Components/TechStack/TechStack.jsx
@@ -6,17 +6,17 @@ const TechStack = () => {
       <h1 className="lg:text-center text-3xl lg:text-6xl font-semibold mb-8 lg:mb-12">
         Tech Stack
       </h1>
-      <div className="flex flex-wrap gap-16 text-center">
+      <div className="grid grid-cols-3 lg:flex lg:flex-wrap gap-4 lg:gap-16 text-center">
         {skills.map((skill, index) => {
           return (
-            <div key={index} className="grid gap-2 items-center">
+            <div key={index} className="grid  gap-2  lg:gap-0 place-content-center">
               <div>
                 <img
                   src={skill.icon_link}
-                  className="w-16 lg:w-28 h-28"
+                  className="w-16 lg:w-28 h-16 lg:h-28"
                   alt=""
                 />
-                <p className="text-sm lg:text-lg font-semibold">{skill.name}</p>
+                <p className="text-sm lg:text-lg  font-semibold">{skill.name}</p>
               </div>
             </div>
           );
